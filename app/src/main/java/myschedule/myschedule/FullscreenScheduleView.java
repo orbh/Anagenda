68c93ea3d714d8fa355861e1e821e982390ec783
Olle <olle.goblirsch@gmail.com>
2016-02-25 12:32:29 +0100

    New view + start of navigation drawer

---
+++ app/src/main/java/myschedule/myschedule/FullscreenScheduleView.java
@@ -0,0 +1,14 @@
+package myschedule.myschedule;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FullscreenScheduleView extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fullscreen_schedule_layout);
+    }
+
+}
--- app/src/main/java/myschedule/myschedule/MySchedules.java
+++ app/src/main/java/myschedule/myschedule/MySchedules.java
@@ -2,12 +2,17 @@ package myschedule.myschedule;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MySchedules extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_schedules);
+        toolbar =(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
