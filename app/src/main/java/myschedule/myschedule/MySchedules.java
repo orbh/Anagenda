f7016575046a2b2b12c423541d4e9aab4b5eb514
Olle <olle.goblirsch@gmail.com>
2016-02-25 14:46:34 +0100

    Navigation drawer everywhere

--- app/src/main/java/myschedule/myschedule/MySchedules.java
+++ app/src/main/java/myschedule/myschedule/MySchedules.java
@@ -1,18 +1,28 @@
 package myschedule.myschedule;
 
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MySchedules extends AppCompatActivity {
 
     Toolbar toolbar;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_schedules);
         toolbar =(Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
+
+    public void ChangeActivity(View v){
+        Intent intent = new Intent(this, FullscreenScheduleView.class);
+        startActivity(intent);
+    }
+
 }
