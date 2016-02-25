20eefaaafbc690ff8647fa7d839eacc18cafc102
Olle <olle.goblirsch@gmail.com>
2016-02-25 11:01:47 +0100

    Initial commit

---
+++ app/src/main/java/myschedule/myschedule/MySchedules.java
@@ -0,0 +1,13 @@
+package myschedule.myschedule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MySchedules extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_schedules);
+    }
+}
