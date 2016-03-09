411e9f50a9407db915c1551b9edc891373ce2cfc
Olle <olle.goblirsch@gmail.com>
2016-03-09 14:42:04 +0100

    Added locale to schedule view

--- app/src/main/java/myschedule/myschedule/CustomScheduleAdapter.java
+++ app/src/main/java/myschedule/myschedule/CustomScheduleAdapter.java
@@ -22,18 +22,23 @@ public class CustomScheduleAdapter extends ArrayAdapter<Element>{
     public View getView(int position, View convertView, ViewGroup parent) {
 
         //ToDo Look up ViewHolder to speed up the ListView
-
         LayoutInflater inflater = LayoutInflater.from(getContext());
         View customView = inflater.inflate(R.layout.schedule_layout, parent, false);
 
         Element element = getItem(position);
         TextView description = (TextView) customView.findViewById(R.id.description_textview);
         TextView weekday = (TextView) customView.findViewById(R.id.weekday_textview);
+
+        //ToDo Date doesn't get a date if there are simultaneous classes on the same day
         TextView date = (TextView) customView.findViewById(R.id.date_textview);
+        TextView locale = (TextView) customView.findViewById(R.id.locale_textview);
 
         description.setText(element.child(9).text());
         weekday.setText(element.child(1).text());
         date.setText(element.child(2).text());
+
+        //ToDo If locale is tentamen it it too long. Consider using a shortener or make the layout bigger
+        locale.setText(element.child(7).text());
         return customView;
     }
 
