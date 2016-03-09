418270c85f083053f6bd5479f5366e1f730e21e0
Olle <olle.goblirsch@gmail.com>
2016-03-09 11:12:53 +0100

    Start of a schedule view + change of colors

--- app/src/main/java/myschedule/myschedule/TESTKLASS.java
+++ app/src/main/java/myschedule/myschedule/TESTKLASS.java
@@ -5,19 +5,26 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class TESTKLASS extends AppCompatActivity {
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
-    TextView twTitle;
+    ListView lwSchedule;
+
+    ArrayAdapter scheduleAdapter;
+    List<String> postList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +32,48 @@ public class TESTKLASS extends AppCompatActivity {
         setContentView(R.layout.testklass_layout);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        twTitle = (TextView) findViewById(R.id.twScheduleTitle);
 
+        lwSchedule = (ListView)findViewById(R.id.lwSchedule);
+        scheduleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, postList);
+        lwSchedule.setAdapter(scheduleAdapter);
+
+        LoadSchedule();
+    }
+
+    public void LoadSchedule() {
         try {
             Document document = new AsyncHelper().execute(getResources().getString(R.string.default_schedule)).get();
-            Elements titles = document.getElementsByClass("data");
-            for (Element title : titles) {
-                twTitle.setText(title.text());
+
+            //Fetches table with only schedule rows
+            Elements posts = document.select("table.schemaTabell > tbody > tr.data-white, tr.data-grey");
+
+            //Fetches the description of the post and adds it to the postList
+            for (Element element : posts) {
+                postList.add(element.child(9).text());
             }
-        } catch (InterruptedException e) {
+
+            //Sets the name of toolbar
+            Element title = document.select("td.big2 > table > tbody > tr > td").get(1);
+            String input = title.text();
+            String output = input.substring(input.indexOf(",") + 1);
+
+            assert getSupportActionBar() != null;
+            if(output.equals("")){
+                getSupportActionBar().setTitle("Schema");
+            }
+            else getSupportActionBar().setTitle(output);
+        }
+        catch (InterruptedException e) {
             e.printStackTrace();
             Log.e("InterruptedException", "InterruptedException" + e);
-        } catch (ExecutionException e) {
+        }
+        catch (ExecutionException e) {
             e.printStackTrace();
             Log.e("ExecutionException", "ExecutionExeption" + e);
+        }
+        catch (NullPointerException e) {
+            e.printStackTrace();
+            Log.e("NullPointerExeption", "NullPointerExeption" + e);
         }
     }
 
