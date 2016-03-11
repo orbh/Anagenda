c159debd43545758e7c58db83a20c0145efcd3c5
Olle <olle.goblirsch@gmail.com>
2016-03-11 13:30:58 +0100

    Updated layout for schedule + new method for setting color for imageview

--- app/src/main/java/myschedule/myschedule/CustomScheduleAdapter.java
+++ app/src/main/java/myschedule/myschedule/CustomScheduleAdapter.java
@@ -6,12 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.jsoup.nodes.Element;
 
 import java.util.List;
+import java.util.Random;
 
 public class CustomScheduleAdapter extends ArrayAdapter<Element>{
 
@@ -33,10 +35,22 @@ public class CustomScheduleAdapter extends ArrayAdapter<Element>{
         TextView wdAndDate = (TextView) customView.findViewById(R.id.wd_and_date_textview);
         TextView locale = (TextView) customView.findViewById(R.id.locale_textview);
         TextView time = (TextView) customView.findViewById(R.id.time_textview);
+        ImageView color = (ImageView) customView.findViewById(R.id.row_color_imageview);
 
-        //Makes half of the rows grayish
+        //Makes half of the rows grayish and makes the color on the side
         if((position % 2) == 0) {
             rowLayout.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorRowGray));
+        }
+
+        //Sets random color for side
+        Random numberGenerator = new Random();
+        int number = numberGenerator.nextInt(3);
+        if (number == 0) {
+            color.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        } else if (number == 1) {
+            color.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
+        } else if (number == 2) {
+            color.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorTextSecondary));
         }
 
         //Highlights posts with tenta or omtenta as locale
@@ -53,6 +67,7 @@ public class CustomScheduleAdapter extends ArrayAdapter<Element>{
         String mergedWdAndDate = element.child(1).text() + ", " + element.child(2).text();
 
         //ToDo Make the comma go away then weekday and date are missing
+
 
         wdAndDate.setText(mergedWdAndDate);
         description.setText(element.child(9).text());
--- app/src/main/java/myschedule/myschedule/TESTKLASS.java
+++ app/src/main/java/myschedule/myschedule/TESTKLASS.java
@@ -1,18 +1,10 @@
 package myschedule.myschedule;
 
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.widget.ListView;
-
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.DrawerBuilder;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -27,8 +19,6 @@ public class TESTKLASS extends AppCompatActivity {
     Toolbar toolbar;
     ListView lwSchedule;
 
-    Drawer navigationDrawer;
-
     CustomScheduleAdapter customScheduleAdapter;
 
     //Contains all "rows" of posts
@@ -42,27 +32,6 @@ public class TESTKLASS extends AppCompatActivity {
         //Toolbar
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        //Navigation drawer
-        //ToDo Manually set icons to nav items
-        PrimaryDrawerItem navMySchedules = new PrimaryDrawerItem().withName(R.string.navigation_drawer_my_schedules).withIcon(ContextCompat.getDrawable(this, R.drawable.ic_date_range_black_18dp));
-        PrimaryDrawerItem navFullscreenScheduleView = new PrimaryDrawerItem().withName(R.string.navigation_drawer_fullscreen_schedule_view);
-        SecondaryDrawerItem navTestclass = new SecondaryDrawerItem().withName(R.string.navigation_drawer_testclass);
-        navigationDrawer = new DrawerBuilder()
-                .withActivity(this)
-                .withToolbar(toolbar)
-                .addDrawerItems(navMySchedules,navFullscreenScheduleView, new DividerDrawerItem(),
-                        navTestclass)
-                .withOnDrawerNavigationListener(new Drawer.OnDrawerNavigationListener() {
-                    @Override
-                    public boolean onNavigationClickListener(View clickedView) {
-
-                        //ToDo Code for pressing items in Nav Drawer below
-
-                        return false;
-                    }
-                }).build();
-        navigationDrawer.addStickyFooterItem(new PrimaryDrawerItem().withName(R.string.navigation_drawer_settings).withIcon(ContextCompat.getDrawable(this, R.drawable.ic_settings_black_18dp)));
 
         //Schedule with custom adapter
         lwSchedule = (ListView)findViewById(R.id.lwSchedule);
