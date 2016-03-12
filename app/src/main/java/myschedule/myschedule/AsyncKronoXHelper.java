de124b06f249c2071f49c47ddc76b58507e91d2c
Olle <olle.goblirsch@gmail.com>
2016-03-12 17:46:01 +0100

    Changed name of async and started method to save a schedule

---
+++ app/src/main/java/myschedule/myschedule/AsyncKronoXHelper.java
@@ -0,0 +1,28 @@
+package myschedule.myschedule;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+class AsyncKronoXHelper extends AsyncTask<String, Void, Document>{
+
+    //Async task that fetches the schedule
+    //Connects to specified url and returns a document
+    protected Document doInBackground(String... urls) {
+        try {
+            return Jsoup.connect(urls[0]).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("IOExeption", "IOExeption" + e);
+            return null;
+        }
+    }
+
+    //For tasks that have to happen after doInBackground
+    protected void onPostExecute(Document document) {
+    }
+}
